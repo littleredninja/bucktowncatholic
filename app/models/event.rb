40291aa1caab0905281287f88cc6e93871ae3d9a
Joey Chamberlin <jchamberlin@ascensionpress.com>
2015-11-11 14:10:22 -0600

    Add methods for allowing time to be added as a string

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,18 @@ class Event < ActiveRecord::Base
   def self.upcoming
     where("date >= ?", Date.today).order("date ASC")
   end
+
+  def time_string
+    time.to_s(:db)
+  end
+
+  def time_string=(time_str)
+    self.time = Time.parse(time_str)
+  rescue ArgumentError
+    @time_invalid = true
+  end
+
+  def validate
+    errors.add(:time, "is invalid")
+  end
 end
