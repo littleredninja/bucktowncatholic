e8232987b953f6ab07eaffefe166575d35b7164b
Joey Chamberlin <jchamberlin@ascensionpress.com>
2015-11-11 14:09:32 -0600

    Add support for timezones

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 helpers do
   def stringify_date(event)
     if event.time.strftime("%M") == "00"
-      "#{event.date.strftime('%A, %b %-d')} at #{event.time.strftime('%l %P')}"
+      "#{event.date.strftime('%A, %b %-d')} at #{event.time.localtime("-06:00").strftime('%l %P')}"
     else
-      "#{event.date.strftime('%A, %b %-d')} at #{event.time.strftime('%l:%M %P')}"
+      "#{event.date.strftime('%A, %b %-d')} at #{event.time.localtime("-06:00").strftime('%l:%M %P')}"
     end
   end
 end
