2386e50b5daf081c044349a96c8710cdf13c2163
Joey Chamberlin <jchamberlin@ascensionpress.com>
2016-05-02 08:46:09 -0500

    Add production database adapter

--- environments.rb
+++ environments.rb
@@ -4,14 +4,28 @@
 # end
 
 configure :development do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///bucktowncatholic_development')
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///bucktowncatholic_development')
 
- ActiveRecord::Base.establish_connection(
+  ActiveRecord::Base.establish_connection(
    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
    :host     => db.host,
    :username => db.user,
    :password => db.password,
    :database => db.path[1..-1],
    :encoding => 'utf8'
- )
+   )
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///bucktowncatholic_production')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :encoding => 'utf8',
+    :database => <%= ENV['RDS_DB_NAME'] %>,
+    :username => <%= ENV['RDS_USERNAME'] %>,
+    :password => <%= ENV['RDS_PASSWORD'] %>,
+    :host     => <%= ENV['RDS_HOSTNAME'] %>,
+    :port     => <%= ENV['RDS_PORT'] %>
+  )
 end
